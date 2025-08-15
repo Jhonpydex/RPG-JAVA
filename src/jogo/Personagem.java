d07a4952507e870a937f9decb12d6a8fdae5dad6
Jhonpydex <abreucotrena@gmail.com>
2025-08-15 18:40:21 -0300

    Update Personagem.java

--- src/jogo/Personagem.java
+++ src/jogo/Personagem.java
@@ -1,24 +1,4 @@
 package jogo;
-/*Etapa 1: Classe Personagem (Base)
-🎯 Objetivo:
-Criar a classe base com atributos comuns a todos os personagens (jogador e inimigos).
-📋 O que ela precisa conter:
-Atributos:
-nome (String)
-classe (String)
-ataque (int)
-defesa (int)
-hp (int)
-Método:
-mostrarStatus() → exibe os dados do personagem.
-
-🧠 Dica de raciocínio:
-Pensa que essa classe é como um molde genérico. Tudo que é comum a
-qualquer personagem vai aqui.
-Você pode usar protected nos atributos se quiser que as classes
-filhas acessem diretamente, ou private com
-getters/setters se quiser aplicar encapsulamento.
-*/
 //1
 public class Personagem {
     public String nome;
